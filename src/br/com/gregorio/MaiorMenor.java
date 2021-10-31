c76fa52cbe0e0bd5376fccc247ba826c4759a531
Gregorio Santos <gregoriohd@gmail.com>
2021-10-31 14:16:28 -0300

    classe MenorMaior criada

---
+++ src/br/com/gregorio/MaiorMenor.java
@@ -0,0 +1,12 @@
+package br.com.gregorio;
+
+public class MaiorMenor {
+	
+	public int maior(int a, int b) {
+		return Math.max(a, b);
+	}
+	public int menor(int a, int b) {
+		return Math.min(a, b);
+	}
+
+}
