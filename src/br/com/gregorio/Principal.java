ecf25a8b959d75faa2d6dbb061bf601af1ca55bb
Gregorio Santos <gregoriohd@gmail.com>
2021-10-31 14:03:06 -0300

    classe Principal atualizada

--- src/br/com/gregorio/Principal.java
+++ src/br/com/gregorio/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Neste Primeiro exemplo utilizaremos funcões matematicas pronta do java");
 	}
 
 }
