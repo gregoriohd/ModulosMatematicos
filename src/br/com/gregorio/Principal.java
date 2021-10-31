fcfc0369be4d686f4d1c1461d794791983ea4291
Gregorio Santos <gregoriohd@gmail.com>
2021-10-31 14:20:47 -0300

    obejto MaiorMenor criado na classe principal

--- src/br/com/gregorio/Principal.java
+++ src/br/com/gregorio/Principal.java
@@ -5,6 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Neste Primeiro exemplo utilizaremos funcões matematicas pronta do java");
+		
+		System.out.println("Qual o maior entre dois numero: "+ new MaiorMenor().maior(10, 20) );
+		System.out.println("Qual o menor entre dois numero: "+ new MaiorMenor().menor(110, 200) );
 	}
 
 }
